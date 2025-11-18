4ae46a6d1a1d74bde02312a54b2a7b93ece4b812
bennytampubolon-ui <bennytampubolont@gmail.com>
2025-11-18 21:13:44 +0700

    BENNY
    SELESAI

--- TLAST.java
+++ TLAST.java
@@ -1,2 +1,23 @@
-//Nama - NIM
+//Benny Tampubolon - 12S25044
+import java.util.*;
+import java.lang.Math;
+
+public class JavaApplication {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int totDat;
+        String command;
+        String[] desk = new String[10], cde = new String[10], matkul = new String[10], dosen = new String[10], deadln = new String[10], ketID = new String[10], grade = new String[10], hari = new String[10], stts = new String[10], prioritas = new String[10];
+
+        totDat = 0;
+        while (!command.equals("---")) {
+            command = input.nextLine();
+            if (command.equals("AddTask")) {
+                AddTask();
+            }
+        }
+    }
+}
+
 
